3f63a36ac6ddff6dbd7879ae7f904b11c8f6d43b
Jonatas Ferreira <jonatas.lima@ccc.ufcg.edu.br>
2022-06-27 12:30:02 -0300

    first commit

---
+++ src/main/java/com/qgasosa/backend/QgasosaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qgasosa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QgasosaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QgasosaBackendApplication.class, args);
+    }
+
+}
