54f225a1a542e74fee040b6ac6cb3aab702206f0
Jonatas Ferreira <jonatas.lima@ccc.ufcg.edu.br>
2022-07-11 13:22:59 -0300

    refactor: consertando nome errado e tratando a exceção

--- src/main/java/com/qgasosa/backend/exception/gas_station/GasStationAlreadyExistsException.java
+++ src/main/java/com/qgasosa/backend/exception/gas_station/GasStationAlreadyExistsException.java
@@ -2,11 +2,11 @@ package com.qgasosa.backend.exception.gas_station;
 
 import com.qgasosa.backend.exception.common.ConflictException;
 
-public class GasSatationAlreadyExists extends ConflictException {
+public class GasStationAlreadyExistsException extends ConflictException {
 
     private static final String GAS_STATION_WITH_NAME_ALREADY_EXISTS = "Gas station with name '%s' already exists.";
 
-    public GasSatationAlreadyExists(String name) {
+    public GasStationAlreadyExistsException(String name) {
         super(String.format(GAS_STATION_WITH_NAME_ALREADY_EXISTS, name));
     }
 }
