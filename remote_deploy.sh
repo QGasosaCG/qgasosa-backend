b478be0c6b9b3a104aee4c430a5e61b58e78333f
Jonatas Ferreira <jonatas.lima@ccc.ufcg.edu.br>
2022-07-31 20:34:21 -0300

    fix: corrigindo script de deploy

--- remote_deploy.sh
+++ remote_deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker rm -f app && docker run --name app --network host -d jonatasflima/qgasosa-backend:$1
+docker rm -f app 
+
+docker pull jonatasflima/qgasosa-backend:$1 && docker run --name app --network host -d jonatasflima/qgasosa-backend:$1
